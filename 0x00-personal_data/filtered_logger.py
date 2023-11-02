760da1d012ccb347434f0882ce2273b3472c07d9
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-11-02 09:51:56 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -73,6 +73,7 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """
+    A function that returns a connector to the database
     """
     user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
